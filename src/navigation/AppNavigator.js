8a79b1d3848e3e383f80ebc275a8f37d61294e15
Randy <randywardhana37@gmail.com>
2021-08-11 13:14:19 +0700

    fix navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
 
 import SplashScreen from '../components/SplashScreen'
 import AllScreen from '../components/AllScreen'
 import WebviewScreen from '../components/WebviewScreen'
 
+const MainNavigator = createStackNavigator({
+  App: { screen: AllScreen },
+  Webview: { screen: WebviewScreen },
+}, {
+  initialRouteName: 'App',
+  headerMode: 'none'
+})
 
 const RootNavigator = createSwitchNavigator({
+  MainNavigator: MainNavigator,
   Splash: SplashScreen,
-  App: AllScreen,
-  Webview: WebviewScreen
 }, {
   initialRouteName: 'Splash'
 })
